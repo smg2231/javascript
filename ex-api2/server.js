8463483cb2d6c45d5d0fde8bd1adc80f61cddaba
smg2231 <flower91634@outlook.com>
2025-10-13 08:09:02 -0400

    API End&Front

--- ex-api2/server.js
+++ ex-api2/server.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/weather', (req, res) => {
   res.json({
     zip: req.query.zipcode,
-    temp: Math.floor(Math.random() * (95-50+1)) + 50
-  })
-})
+    temp: Math.floor(Math.random() * (95 - 50 + 1)) + 50
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
